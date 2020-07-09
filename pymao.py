1f70db7ba6659db83b021d52e884f4195754f22b
IZAY01 <1906826229@qq.com>
2020-07-09 21:21:08 +0800

    v 0.1

---
+++ pymao.py
@@ -0,0 +1,43 @@
+import os
+
+
+
+def main_arena_offset( libc : str ) -> int :
+    
+    file_libc = get_output("file " + libc)
+    if( file_libc.find("64-bit") == -1 ):
+        return get_32_bit_main_arena_offset( libc )
+    else:
+        return get_64_bit_main_arena_offset( libc )
+
+
+
+def get_32_bit_main_arena_offset( libc : str ) -> int :
+    
+    malloc_hook_str = get_output("objdump -j .data -d " + libc + "|grep __malloc_hook |cut -d' ' -f 1")
+    malloc_hook = int(malloc_hook_str, 16)
+    return malloc_hook + 0x18
+
+
+
+def get_64_bit_main_arena_offset( libc : str) -> int :
+    
+    malloc_hook_str = get_output("objdump -j .data -d " + libc + "|grep __malloc_hook |cut -d' ' -f 1")
+    realloc_hook_str = get_output("objdump -j .data -d " + libc + "|grep __realloc_hook |cut -d' ' -f 1")
+    malloc_hook = int(malloc_hook_str, 16)
+    realloc_hook = int(realloc_hook_str, 16)
+    return 3*malloc_hook - 2*realloc_hook
+
+
+
+def get_output( command : str ) -> str :
+    p = os.popen( command )
+    s = p.read()
+    p.close()
+    return s
+
+
+
+if __name__ == "__main__" :
+
+    print(hex(main_arena_offset("libc.so")))

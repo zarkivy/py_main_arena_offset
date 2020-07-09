34c069fe07ccb2a2351722d741d46a8e2b3218e7
IZAY01 <1906826229@qq.com>
2020-07-09 21:27:27 +0800

    v 0.1.1

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pymao",
+    version="0.1",
+    description="Get main_arena offset of a given libc with python",
+    author="IZAYOI",
+    author_email="z1906826229@gmail.com",
+    platforms=["any"],
+    license="BSD",
+    url="https://github.com/IZAY01/py_main_arena_offset",
+    packages=find_packages(), )
+
